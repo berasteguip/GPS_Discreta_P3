b91ec53bb3557f0901137ef6f105738b7af241a4
Pablo <202311460@alu.comillas.edu>
2024-11-20 16:47:23 +0100

    new

--- Materiales Práctica 3-20241113/callejero.py
+++ Materiales Práctica 3-20241113/callejero.py
@@ -14,14 +14,17 @@ Librería con herramientas y clases auxiliares necesarias para la representació
 
 Complétese esta descripción según las funcionalidades agregadas por el grupo.
 """
-print('hola')
 
 import osmnx as ox
 import networkx as nx
 import pandas as pd
 import regex as re
+import os
 
 from typing import Tuple
+from Levenshtein import distance as string_distance
+
+MAX_DISTANCE = 0.15     # Encontrar distancia máxima ideal
 
 STREET_FILE_NAME="direcciones.csv"
 
@@ -91,9 +94,17 @@ def carga_callejero() -> pd.DataFrame:
     callejero['FLOAT_LONGITUD'] = callejero['LONGITUD'].apply(coord_to_float)
 
     # Nombre completo de la calle
-    callejero['NOMBRE_COMPLETO'] = (callejero['VIA_CLASE'] + callejero['VIA_PAR'] + callejero['VIA_NOMBRE']).strip()
+    replace_nans = lambda par: "" if str(par).lower() == 'nan' else str(par) + " "
+
+    callejero['VIA_PAR'] = callejero['VIA_PAR'].apply(replace_nans)
+    callejero['NOMBRE_COMPLETO'] = callejero['VIA_CLASE'] + " " + callejero['VIA_PAR'] + callejero['VIA_NOMBRE'] + ", " + callejero['NUMERO'].astype(str)
     return callejero
 
+
+def distancia(calle_pedida, calle_iter):
+    VALOR = 0.7 # recortaremos todos los cambios que sean mayores que este valor
+    distancia = min(string_distance(calle_pedida.lower(), calle_iter.lower())/len(calle_pedida), VALOR)
+    return distancia
 
 def busca_direccion(direccion:str, callejero:pd.DataFrame) -> Tuple[float,float]:
     """ Función que busca una dirección, dada en el formato
@@ -109,33 +120,68 @@ def busca_direccion(direccion:str, callejero:pd.DataFrame) -> Tuple[float,float]
     Raises:
         AdressNotFoundError: Si la dirección no existe en la base de datos
     Example:
-        busca_direccion("Calle de Alberto Aguilera, 23", data)=(40.42998055555555,3.7112583333333333)
-        busca_direccion("Calle de Alberto Aguilera, 25", data)=(40.43013055555555,3.7126916666666667)
+        busca_direccion("Calle de Alberto Aguilera, 23", data)=(40.42998055555555,-3.7112583333333333)
+        busca_direccion("Calle de Alberto Aguilera, 25", data)=(40.43013055555555,-3.7126916666666667)
     """
 
+    direccion = direccion.upper()
+    if direccion in callejero['NOMBRE_COMPLETO'].unique():
+        
+        lat = callejero.loc[callejero['NOMBRE_COMPLETO'] == direccion,'FLOAT_LATITUD'].values[0]
+        long = callejero.loc[callejero['NOMBRE_COMPLETO'] == direccion,'FLOAT_LONGITUD'].values[0]
+        return lat, long
 
-
-    callejero['VIA_PAR'] = callejero['VIA_PAR'].str.strip()     # Evitamos espacios erróneos antes y después de las partículas
-    calle, numero = [chain.strip() for chain in direccion.split(',')]
-
-    callejero.loc[(callejero['NOMBRE_COMPLETO'] == calle)]
-
-    
+    else:
+        callejero_dist = callejero.copy()
+        callejero_dist['DISTANCIA'] = callejero_dist['NOMBRE_COMPLETO'].apply( lambda x: distancia(direccion, x))
+        callejero_dist = callejero_dist.sort_values(by='DISTANCIA', ascending=True).reset_index()
+        
+        closest = callejero_dist.iloc[0]
+        print(closest['DISTANCIA'])
+        if closest['DISTANCIA'] > MAX_DISTANCE:
+            raise AdressNotFoundError('La dirección proporcionada no es correcta.')
+        else:
+            print(closest['NOMBRE_COMPLETO'])
+            lat = closest['FLOAT_LATITUD']
+            long = closest['FLOAT_LONGITUD']
+            return lat, long
 
 
 
 ############## Parte 4 ##############
 
 
-def carga_grafo() -> nx.MultiDiGraph:
-    """ Función que recupera el quiver de calles de Madrid de OpenStreetMap.
+def carga_grafo() -> nx.DiGraph:
+    """ Función que recupera el quiver de calles de Madrid de OpenStreetMap y lo convierte en un grafo dirigido sin bucles.
     Args: None
     Returns:
-        nx.MultiDiGraph: Quiver de las calles de Madrid.
+        nx.DiGraph: Grafo dirigido sin bucles de las calles de Madrid.
     Raises:
         ServiceNotAvailableError: Si no es posible recuperar el grafo de OpenStreetMap.
     """
-    pass
+
+    graph_file = "madrid.graphml"
+    
+    if os.path.exists(graph_file):
+        # Cargar el grafo desde el archivo
+        grafo = ox.load_graphml(graph_file)
+    else:
+        # Descargar el grafo de OpenStreetMap
+        grafo = ox.graph_from_place("Madrid, Spain", network_type='drive')
+
+        # Guardar el grafo en un archivo
+        ox.save_graphml(grafo, graph_file)
+    
+    # Convertir el multidigrafo en un grafo dirigido
+    grafo_dirigido = ox.convert.to_digraph(grafo)
+    
+    # Eliminar bucles
+    grafo_dirigido.remove_edges_from(nx.selfloop_edges(grafo_dirigido))
+    
+    # Pintar el grafo
+    ox.plot_graph(grafo_dirigido, node_size=0, edge_linewidth=0.5)
+    
+    return grafo_dirigido
 
 
 def procesa_grafo(multidigrafo:nx.MultiDiGraph) -> nx.DiGraph:
